7812057347c00db3e2ee46d9798b2649e6eddb0f
tkrause <tkrause@1ab5e448-18ff-0310-9644-db632cb2dd4a>
2005-10-13 05:58:54 +0000

    - changed my e-mail

--- trunk/dicml-conv/src/dicmlconv/Main.java
+++ trunk/dicml-conv/src/dicmlconv/Main.java
@@ -27,7 +27,7 @@ public class Main {
     {
       System.out.print("\ndicml-conv vers. 0.01\n"+
                        "---------------------\n\n" +
-                       "usage: ask me: tho.krause@gmail.com\n\n");
+                       "usage: ask me: tkrause@developer.berlios.de\n\n");
     }
     
     /**
