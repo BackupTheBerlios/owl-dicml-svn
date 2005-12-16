2595e1c79565028b385b6b30a16b94847429c52a
tkrause <tkrause@1ab5e448-18ff-0310-9644-db632cb2dd4a>
2005-12-16 15:12:30 +0000

    - passing argument from shell/cmd to owl

--- trunk/owl-reader/owl.bat
+++ trunk/owl-reader/owl.bat
@@ -2,5 +2,5 @@ REM starts owl, please don't start owl.jar directly or make sure that this
 REM directory is the working-directory
 ECHO OFF
 cd $INSTALL_PATH
-START javaw -jar bin/owl.jar
+START javaw -jar bin/owl.jar %1
 EXIT
--- trunk/owl-reader/owl.sh
+++ trunk/owl-reader/owl.sh
@@ -1,4 +1,4 @@
 # starts owl, please don't start owl.jar directly or make sure that this 
 # directory is the working-directory
 cd $INSTALL_PATH
-java -jar bin/owl.jar
+java -jar bin/owl.jar $1
