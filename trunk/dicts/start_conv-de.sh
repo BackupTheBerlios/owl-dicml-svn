7a3580b0b5c479b0d6f8ffde6c1fe1f2b5fb2866
tkrause <tkrause@1ab5e448-18ff-0310-9644-db632cb2dd4a>
2006-08-12 18:25:45 +0000

    - added some files which I forget to add in the past

---
+++ trunk/dicts/start_conv-de.sh
@@ -0,0 +1,1 @@
+java -classpath ../dicml-conv/build/classes/  dicmlconv.Main -chemnitz de-en.txt de-en.dicml left head-de-en.txt
---
+++ trunk/dicts/start_conv-en.sh
@@ -0,0 +1,1 @@
+java -classpath ../dicml-conv/build/classes/  dicmlconv.Main -chemnitz de-en.txt en-de.dicml right head-en-de.txt
