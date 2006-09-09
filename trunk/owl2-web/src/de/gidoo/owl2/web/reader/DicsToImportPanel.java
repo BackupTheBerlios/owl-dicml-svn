25bda9fb2fe5b170b59f05af74f2a1e9e7683f17
tkrause <tkrause@1ab5e448-18ff-0310-9644-db632cb2dd4a>
2006-09-09 13:55:13 +0000

    - list dictionaries available for import - edited stylesheet so that tabs are looking a little bit more tab-like

--- trunk/owl2-web/src/de/gidoo/owl2/web/reader/DicsToImportPanel.java
+++ trunk/owl2-web/src/de/gidoo/owl2/web/reader/DicsToImportPanel.java
@@ -8,6 +8,16 @@
 
 package de.gidoo.owl2.web.reader;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import wicket.model.*;
+import wicket.markup.html.basic.*;
+import wicket.markup.html.link.Link;
+import wicket.markup.html.list.ListItem;
+import wicket.markup.html.list.ListView;
+
 /**
  * Presents a list of all dicML files inside a special folder of the server
  * and let the admin import them.
@@ -15,12 +25,64 @@ package de.gidoo.owl2.web.reader;
  */
 public class DicsToImportPanel extends wicket.markup.html.panel.Panel {
   
+  private static final List _dictList = new Vector();
+  private final ListView lstDicts;
+  
   /**
    * Creates a new instance of DicsToImportPanel
    */
   public DicsToImportPanel(String id) 
   {
     super(id);
+    
+    _dictList.clear();
+    
+    updateDicList();
+        
+    lstDicts = new ListView("listView", _dictList) 
+    {
+      protected void populateItem(ListItem item) 
+      {
+        final File f = (File) item.getModelObject();
+        item.add(new Label("lblFileName", new Model(f.getName())));
+        item.add(new Link("lnkImport") {
+          public void onClick() 
+          {
+            importDict(f.getAbsolutePath());
+          }
+        });
+      }
+    };
+    
+    add(lstDicts);
+    
+  }
+  
+  private void updateDicList()
+  {
+    // iterate through the dirctory
+    File dictDir = new File(OwlApp.realPathToContext + "WEB-INF/dicts");
+    if(!dictDir.exists())
+    {
+      dictDir.mkdir();
+    }
+    
+    if(dictDir.isDirectory())
+    {
+      File[] subfiles = dictDir.listFiles();
+      for(int i=0; i < subfiles.length; i++)
+      {
+        if(subfiles[i].getName().endsWith(".dicml"))
+        {
+          _dictList.add(subfiles[i]);
+        }
+      }
+    }
+  }
+  
+  private final void importDict(String path)
+  {
+    // do something usefull
   }
   
 }
