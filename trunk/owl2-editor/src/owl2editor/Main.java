196fc6d13476677d66ea2f6f232acbc010e29a39
tkrause <tkrause@1ab5e448-18ff-0310-9644-db632cb2dd4a>
2007-02-12 18:40:34 +0000

    empty project for editor

---
+++ trunk/owl2-editor/src/owl2editor/Main.java
@@ -0,0 +1,29 @@
+/*
+ * Main.java
+ *
+ * Created on 12. Februar 2007, 19:32
+ */
+
+package owl2editor;
+
+/**
+ *
+ * @author Thomas Krause
+ */
+public class Main
+{
+  
+  /** Creates a new instance of Main */
+  public Main()
+  {
+  }
+  
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args)
+  {
+    // TODO code application logic here
+  }
+  
+}
